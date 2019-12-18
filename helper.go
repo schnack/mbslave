cba4a328eda7aee70691c69f7d1402173e16ef24
Mikhail Stolyarov <schnack.desu@gmail.com>
2019-12-18 20:11:55 +0300

    del old function

--- helper.go
+++ helper.go
@@ -1,16 +1,5 @@
 package mbslave
 
-import "time"
-
-func RtuFrameDelay(baudRate int) (frameDelay time.Duration) {
-	if baudRate <= 0 || baudRate > 19200 {
-		frameDelay = 1750 * time.Microsecond
-	} else {
-		frameDelay = time.Duration(35000000/baudRate) * time.Microsecond
-	}
-	return
-}
-
 func ExceptionFunction(function uint8) uint8 {
 	return function | 1<<7
 }
